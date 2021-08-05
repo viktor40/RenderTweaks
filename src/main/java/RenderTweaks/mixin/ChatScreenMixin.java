35e897f04bd217bd59ee96fabe30c28afa7a40eb
viktor40 <vannieuwenhuize.viktor@gmail.com>
2021-08-05 15:28:23 +0200

    Added new mixins classes for future features

---
+++ src/main/java/RenderTweaks/mixin/ChatScreenMixin.java
@@ -0,0 +1,8 @@
+package RenderTweaks.mixin;
+
+import net.minecraft.client.gui.screen.ChatScreen;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ChatScreen.class)
+public class ChatScreenMixin {
+}
---
+++ src/main/java/RenderTweaks/mixin/VideoOptionsScreenMixin.java
@@ -0,0 +1,8 @@
+package RenderTweaks.mixin;
+
+import net.minecraft.client.gui.screen.option.VideoOptionsScreen;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(VideoOptionsScreen.class)
+public class VideoOptionsScreenMixin {
+}
