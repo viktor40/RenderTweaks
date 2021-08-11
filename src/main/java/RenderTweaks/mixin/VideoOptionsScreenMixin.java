07a131c5476fe675389ae97954ad16349dcd5d4a
viktor40 <vannieuwenhuize.viktor@gmail.com>
2021-08-11 11:14:23 +0200

    added mixin to add rendertweaks option button to the VideoOptionsScreen

--- src/main/java/RenderTweaks/mixin/VideoOptionsScreenMixin.java
+++ src/main/java/RenderTweaks/mixin/VideoOptionsScreenMixin.java
@@ -2,7 +2,14 @@ package RenderTweaks.mixin;
 
 import net.minecraft.client.gui.screen.option.VideoOptionsScreen;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(VideoOptionsScreen.class)
 public class VideoOptionsScreenMixin {
+    @Inject(method = "init", at = @At("TAIL"))
+    private void addOptionsButton(CallbackInfo ci) {
+
+    }
 }
